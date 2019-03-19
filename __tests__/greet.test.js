2bd906ba7db3aea301baf7d585e35cb4a57e202b
Joseph <z@charmed.tech>
2019-03-18 21:02:05 -0700

    Use faker for tests

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -2,24 +2,28 @@
 
 const greet = require('../lib/greet.js');
 
+const { boolean, number, objectElement, words } = require('faker').random;
+
 describe('`greet` Function', () => {
   it('should return `null` if it receives an undefined argument', () => {
     expect(greet(undefined)).toBeNull();
   });
   it('should return `null` if it receives a number argument', () => {
-    expect(greet(1)).toBeNull();
+    expect(greet(number())).toBeNull();
   });
   it('should return `null` if it receives a Boolean argument', () => {
-    expect(greet(false)).toBeNull();
+    expect(greet(boolean())).toBeNull();
   });
   it('should return `null` if it receives an Object argument', () => {
-    expect(greet({ hello: 'world' })).toBeNull();
+    expect(greet({ hello: objectElement() })).toBeNull();
   });
-  it('should return `null` if it receives a Null argument', () => {
+  it('should return `null` if it receives a null argument', () => {
     expect(greet(null)).toBeNull();
   });
-
+  it('should return a string beginning with `hello ` if it receives a string argument', () => {
+    expect(greet(words())).toMatch(/hello\s[\w\s]*/g);
+  });
   it('should return `hello world` if the argument is `world`', () => {
-    expect(greet('world')).toMatch('hello world');
+    expect(greet('world')).toMatch(/hello world/);
   });
 });
