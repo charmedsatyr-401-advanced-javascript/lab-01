17c179344f5f4f047826213f8c4f58350dcd45b6
Joseph <z@charmed.tech>
2019-03-18 20:48:11 -0700

    Refactor arithmetic with faker

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,54 +1,71 @@
 'use strict';
 
 const arithmetic = require('../lib/arithmetic.js');
+const { number, word } = require('faker').random;
 
-describe('`arithmetic.add` Method', () => {
-  it('should return `null` if it does not receive two numbers for arguments', () => {
-    expect(arithmetic.add('a', 0)).toBeNull();
+let a, b, c, w;
+let nums, mixed;
+
+beforeEach(() => {
+  a = number();
+  b = number();
+  c = number();
+  w = word();
+  nums = [a, b, c];
+  mixed = [a, b, c, w];
+});
+
+describe('`arithmetic.isNum` method', () => {
+  it('should return true if it receives an array of numbers', () => {
+    expect(arithmetic.isNum(nums)).toBeTruthy();
   });
-  it('should returns the sum of its two number arguments', () => {
-    expect(arithmetic.add(2, 4)).toBe(6);
+  it('should return false if it receives an array that contains non-numbers', () => {
+    expect(arithmetic.isNum(mixed)).toBeFalsy();
   });
-  it('should ignore any more than two arguments', () => {
-    expect(arithmetic.add(1, 2, 3, 4)).toBe(3);
+  it('should return false if its argument is not an array', () => {
+    expect(arithmetic.isNum(w)).toBeFalsy();
   });
 });
 
-describe('`arithmetic.subtract` Method', () => {
-  it('should return `null` if it does not receive two numbers for arguments', () => {
-    expect(arithmetic.subtract('a', 0)).toBeNull();
+describe('`arithmetic.add` method', () => {
+  it('should return `null` if its arguments are not all numbers', () => {
+    expect(arithmetic.add(...mixed)).toBeNull();
   });
-  it('should return the difference of its two arguments', () => {
-    expect(arithmetic.subtract(4, 3)).toBe(1);
-  });
-  it('should ignore any more than two arguments', () => {
-    expect(arithmetic.subtract(2, 1, 3, 4)).toBe(1);
+  it('should return the sum of its arguments', () => {
+    const sum = a + b + c;
+    expect(arithmetic.add(...nums)).toBe(sum);
   });
 });
 
-describe('`arithmetic.multiply` Method', () => {
-  it('should return `null` if it does not receive two numbers for arguments', () => {
-    expect(arithmetic.multiply('a', 0)).toBeNull();
+describe('`arithmetic.subtract` method', () => {
+  it('should return `null` if its arguments are not all numbers', () => {
+    expect(arithmetic.subtract(...mixed)).toBeNull();
   });
-  it('should return the product of its two arguments', () => {
-    expect(arithmetic.multiply(4, 3)).toBe(12);
-  });
-  it('should ignore any more than two arguments', () => {
-    expect(arithmetic.multiply(2, 1, 3, 4)).toBe(2);
+  it('should subtract subsequent arguments from the first argument and return the difference', () => {
+    expect(arithmetic.subtract(...nums)).toBe(a - b - c);
   });
 });
 
-describe('`arithmetic.divide` Method', () => {
+describe('`arithmetic.multiply` method', () => {
+  it('should return `null` if its arguments are not all numbers', () => {
+    expect(arithmetic.multiply(...mixed)).toBeNull();
+  });
+  it('should return the product of its arguments', () => {
+    expect(arithmetic.multiply(...nums)).toBe(a * b * c);
+  });
+});
+
+describe('`arithmetic.divide` method', () => {
   it('should return `null` if it does not receive two numbers for arguments', () => {
-    expect(arithmetic.divide('a', 1)).toBeNull();
+    expect(arithmetic.divide(a, w)).toBeNull();
   });
   it('should return `null` if the second argument is 0', () => {
-    expect(arithmetic.divide(1, 0)).toBeNull();
+    expect(arithmetic.divide(a, 0)).toBeNull();
   });
-  it('should return the quotient of its two arguments', () => {
-    expect(arithmetic.divide(100, 50)).toBe(2);
+  it('should return the quotient of the first argument divided by its second argument', () => {
+    expect(arithmetic.divide(a, b)).toBe(a / b);
   });
   it('should ignore any more than two arguments', () => {
-    expect(arithmetic.divide(2, 1, 3, 4)).toBe(2);
+    expect(arithmetic.divide(...nums)).toBe(a / b);
   });
 });
